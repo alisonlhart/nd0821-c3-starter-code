90f588f28c8eb62ac0a59e3eab056c7ec0e5e633
Ali <alhart@redhat.com>
2023-03-08 19:48:08 -0500

    Finishing API

--- starter/main.py
+++ starter/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder 
 from pydantic import BaseModel, Field
 from pathlib import Path
 import pandas as pd
@@ -8,21 +9,21 @@ from src.ml.model import inference, load_model
 from src.ml.data import process_data
 
 class AllData(BaseModel):
-    age: int = Field(example=38)
-    workclass: str = Field(example="Private")
-    fnlgt: int = Field(example=215646)
-    education: str = Field(example="HS-grad")
-    education_num: Union[int, list] = Field(alias="education-num", example=9)
+    age: int = 38
+    workclass: str = "Private"
+    fnlgt: int = 215646
+    education: str = "HS-grad"
+    education_num: int = Field(example=9, alias="education-num")
     marital_status: str = Field(example="Divorced", alias="marital-status")
-    occupation: str = Field(example="Handlers-cleaners")
-    relationship: str = Field(example="Not-in-family")
-    race: str = Field(example="White")
-    sex: str = Field(example="Male")
+    occupation: str = "Handlers-cleaners"
+    relationship: str = "Not-in-family"
+    race: str = "White"
+    sex: str = "Male"
     capital_gain: int = Field(example=0, alias="capital-gain")
     capital_loss: int = Field(example=0, alias="capital-loss")
     hours_per_week: int = Field(example=40, alias="hours-per-week")
     native_country: str = Field(example="United-States", alias="native-country")
-    
+
 cat_features = [
         "workclass",
         "education",
@@ -36,12 +37,12 @@ cat_features = [
 
 app = FastAPI()
 
-@app.post("/infer")
+@app.post("/inferring")
 async def create_item(data: AllData):
     
-    temp_data = data.dict(by_alias=True)
-    df = pd.DataFrame(temp_data, index=[0])
-        
+    data = jsonable_encoder(data)
+    df = pd.DataFrame(data=data, index=[0])
+    
     model = load_model(Path(__file__).parent / f"model/model.pkl")
     encoder = load_model(Path(__file__).parent / f"model/encoder.pkl")
     lb = load_model(Path(__file__).parent / f"model/lb.pkl")
@@ -54,13 +55,32 @@ async def create_item(data: AllData):
         lb=lb   
     )
     
-    infer = inference(model, X_test)
+    infer = inference(model, X_test).tolist()
     
-    return infer
+    return infer  
+
+
+@app.post("/inferring")
+async def create_item(data: AllData):
     
+    data = jsonable_encoder(data)
+    df = pd.DataFrame(data=data, index=[0])
     
+    model = load_model(Path(__file__).parent / f"model/model.pkl")
+    encoder = load_model(Path(__file__).parent / f"model/encoder.pkl")
+    lb = load_model(Path(__file__).parent / f"model/lb.pkl")
     
+    X_test, y_test, encoder_test, lb_test = process_data(
+        df,
+        cat_features,
+        training=False,
+        encoder=encoder,
+        lb=lb   
+    )
     
+    infer = inference(model, X_test).tolist()
+    
+    return infer      
 
 @app.get("/")
 async def welcome():
